d96b81a8ccbe8c519355fb0541c5ffcca7a2b881
yayan2130 <yayan2130@gmail.com>
2020-10-19 20:05:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/modul_3_latihan3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.modul_3_latihan3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
